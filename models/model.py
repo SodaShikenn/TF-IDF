cc9c623a67d59b5344197548b30500e47dddd72b
SodaShikenn <zixuanhe43@gmail.com>
2025-10-08 14:10:50 +0900

    Realize stop words and jieba

--- models/model.py
+++ models/model.py
@@ -1,44 +1,35 @@
 import math
+from .utils import text_cut
+from tqdm import tqdm # 因为语料比较多，所以计算过程很慢，可以使用tqdm库加载一个进度条，了解处理进度。
 
 class TFIDFModel():
     def __init__(self):
         self.all_words = []
         self.vocab = set() # 去重
         self.corpus_words = []
+    
+    def load_corpus(self, corpus):
+        for sentence in corpus:
+            words = text_cut(text)
+            self.all_words += words
+            self.vocab = self.vocab.update(words)
+            self.corpus_words.append(words)
 
-for sentence in corpus:
-    words = sentence.strip().split(' ')
-    corpus_words.append(words)
-    all_words += words
-    vocab.update(words)
+    def compute_df(self,):
+        self.tf_list = []
+        for words in self.corpus_words:
+            tf = {}
+            for word in words:
+                tf[word] = words.count(word) / len(words)
+            self.tf_list.append(tf)
 
-# TF = 某个词在文本中出现的次数 / 句子的总词数
-# 例如：TF(dog) = [1/6, 0/6]
-tf_list = []
-for words in corpus_words:
-    tf = {}
-    for word in words:
-        tf[word] = words.count(word) / len(words)
-    tf_list.append(tf)
-
-# IDF = log(语料库句子总数 / (包含该词的文档数 + 1))
-# 例如：IDF(dog) = log(2/(1+1))
-idf_dict = {}
-N = len(corpus_words)
-for word in vocab:
-    num = sum([1 if word in words else 0 for words in corpus_words])
-    idf_dict[word] = math.log(N/(num+1))
+    def compute_idf(self):
+        self.idf_dict = {}
+        for word in self.vocab:
+            num = sum([1 if word in words else 0 for words in self.corpus_words])
+            self.idf_dict[word] = math.log(len(self.corpus_words) / (num+1))
 
 
-tfidf_list = []
-for tf in tf_list:
-    tfidf = {}
-    for word, tf_val in tf.items():
-        tfidf[word] = tf_val * idf_dict[word]
-    tfidf_list.append(tfidf)
-# print(tfidf_list)
 
-
-for tfidf in tfidf_list:
-    tfidf_topk = sorted([(v,k) for k,v in tfidf.items()], reverse=True)[:3]
-    # print([k for v,k in tfidf_topk])
+for i in tqdm(lst, desc='comment'):
+    pass
